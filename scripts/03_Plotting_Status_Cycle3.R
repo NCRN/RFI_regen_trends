bb44b220281d6a6784fa0a97dc9ba2dc73913f4c
KateMMiller <kathryn_miller@nps.gov>
2022-04-08 08:40:29 -0400

    improved stocking plots

--- scripts/03_Plotting_Status_Cycle3.R
+++ scripts/03_Plotting_Status_Cycle3.R
@@ -83,7 +83,7 @@ c3_stats <- map2_dfr(parks, metrics, function(park, metric){
 })
 
 write.csv(c3_stats, "./results/20220325/EFWG_cycle_3_status.csv", row.names = FALSE)
-#c3_stats <- read.csv(paste0(datapath, "results/EFWG_cycle_3_status.csv"))
+#c3_stats <- read.csv("./results/EFWG_cycle_3_status.csv")
 
 # Plotting function that sorts from high to low and color codes by network
 plot_c3_status <- function(df, met, xlabel, ylabel){
@@ -92,21 +92,22 @@ plot_c3_status <- function(df, met, xlabel, ylabel){
   
   p <- ggplot(df2, 
               aes(x = park_ord, y = mean, fill = as.factor(Network)))+
-    geom_bar(stat = 'identity', aes(fill = as.factor(Network)))+ 
-    geom_errorbar(aes(ymin = lower95, ymax = upper95), color = "#B9B9B9", 
-                  width = 0.1, size = 0.5, na.rm = TRUE)+
+    geom_bar(stat = 'identity', aes(fill = as.factor(Network)), col = "#868686")+ 
+    geom_errorbar(aes(ymin = lower95, ymax = upper95), color = "#868686", 
+                  width = 0.15, size = 0.4, na.rm = TRUE)+
     scale_fill_manual(values = c("ERMN" = "#A5BDCD", "MIDN" = "#E7CDA4", "NCBN" = "#CFB9D9", 
                                  "NCRN" = "#E1E59B", "NETN" = "#AACCA7"), 
                       name = "Network:")+
     theme_bw()+
     labs(x = xlabel, y = ylabel)+
-    theme(axis.text = element_text(size = 11),
+    theme(axis.text = element_text(size = 14),
           axis.text.x = element_text(angle = 45, hjust = 1),
-          axis.title = element_text(size = 11),
-          plot.title = element_text(hjust = 0.5, size = 11, 
+          axis.text.y = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          plot.title = element_text(hjust = 0.5, size = 14, 
                                     margin = margin(t = 10, b = -15)),
-          legend.title = element_text(size = 11),
-          legend.text = element_text(size = 11),
+          legend.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
           panel.background = element_blank(),
           panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(), 
@@ -120,10 +121,11 @@ plot_c3_status <- function(df, met, xlabel, ylabel){
 # Not going to iterate, so I can customize some of the plots before saving.
 
 plot_c3_status(c3_stats, "stock_final", xlabel = NULL, ylabel = "Mean Stocking Index")+
-  geom_hline(yintercept = 25, linetype = 2, lwd = 1, color = '#CD5C5C')+
-  geom_text(x = 39, y = 22.5, label = "Severely Understocked", hjust = 1, size = 3.5, check_overlap = TRUE)+
-  geom_hline(yintercept = 100, linetype = 4, lwd = 1, color = "#228B22")+
-  geom_text(x = 39, y = 104, hjust = 1, label = "Sufficiently Stocked", size = 3.5, check_overlap = TRUE)
+  geom_hline(yintercept = 25, linetype = 2, lwd = 0.75, color = "DimGrey")+ #'#CD5C5C')+
+  geom_text(x = 39, y = 22.5, label = "Critical", hjust = 1, size = 4.5, check_overlap = TRUE, color = "#696969")+
+  geom_text(x = 39, y = 64.5, label = "Caution", hjust = 1, size = 4.5, check_overlap = TRUE, color = "#696969")+
+  geom_hline(yintercept = 100, linetype = 2, lwd = 0.75, color = "DimGrey")+ #"#228B22")+
+  geom_text(x = 39, y = 104, hjust = 1, label = "Acceptable", size = 4.5, check_overlap = TRUE, color = "#696969")
 
 ggsave("./results/20220325/cycle_3_status_plots/Cycle_3_stocking_index.svg", width = 11, height = 8, units = 'in')
 
@@ -179,27 +181,32 @@ head(c3_stats)
 p <- 
   ggplot(dbi_stock_sum,
             aes(x = park_ord, y = prop_stocked, fill = as.factor(Network)))+
-  geom_bar(stat = 'identity', aes(fill = as.factor(Network)))+ 
+  geom_bar(stat = 'identity', aes(fill = as.factor(Network)), color = "#868686")+ 
   scale_fill_manual(values = c("ERMN" = "#A5BDCD", "MIDN" = "#E7CDA4", "NCBN" = "#CFB9D9", 
                                "NCRN" = "#E1E59B", "NETN" = "#AACCA7"), 
                     name = "Network:")+
-  scale_y_continuous(limits = c(0, 0.5), breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5), 
-                     labels = c("0", "10", "20", "30", "40", "50"))+  
+  scale_y_continuous(limits = c(0, 0.7), breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7), 
+                     labels = c("0", "10", "20", "30", "40", "50", "60", "70"))+  
   theme_bw()+
   labs(x = NULL, y = "% Stocked Plots")+
-  theme(axis.text = element_text(size = 11),
+  theme(axis.text = element_text(size = 14),
         axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.title = element_text(size = 11),
-        plot.title = element_text(hjust = 0.5, size = 11, 
+        axis.title = element_text(size = 14),
+        plot.title = element_text(hjust = 0.5, size = 14, 
                                   margin = margin(t = 10, b = -15)),
-        legend.title = element_text(size = 11),
-        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 14),
         panel.background = element_blank(),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.line = element_blank(), 
         legend.position = 'bottom')+
-  geom_hline(yintercept = 0.25, lty = 2)
+  geom_hline(yintercept = 0.32, lty = 2, lwd = 0.75, color = "#696969")+
+  geom_text(x = 39, y = 0.30, label = "Critical", hjust = 1, size = 4.5, check_overlap = TRUE, color = "#696969")+
+  geom_hline(yintercept = 0.66, lty = 2, lwd = 0.75, color = "#696969")+
+  geom_text(x = 39, y = 0.495, label = "Caution", hjust = 1, size = 4.5, check_overlap = TRUE, color = "#696969")+
+  geom_text(x = 39, y = 0.68, hjust = 1, label = "Acceptable", size = 4.5, check_overlap = TRUE, color = "#696969")
+  
 p
 
 ggsave("./results/20220325/cycle_3_status_plots/Pct_stocked_plots.svg", width = 11, height = 8, units = 'in')
